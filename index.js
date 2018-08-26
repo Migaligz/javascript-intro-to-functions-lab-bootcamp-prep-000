38f2fc690770379f4efe21020fc0ac537481e61c
Aaron Isberg <aaronisberg@gmail.com>
2018-08-26 18:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-  return mixedCase.toLowerCase() === mixedCase
+  return string.mixedCase.toLowerCase() === mixedCase
 }
